f3a118b8761f210795d2c0031c7d4f50fad81d4b
bambam29575 <123730907+bambam29575@users.noreply.github.com>
2024-02-05 12:27:42 +0530

    Create CircularQueue.cpp

---
+++ Queue/CircularQueue.cpp
@@ -0,0 +1,65 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+#define MAX 4
+
+int queue[MAX], front = -1, rear = -1, item, i;
+char ch;
+
+void enq();
+void deq();
+void dis();
+void opr();
+
+int main() {
+    opr();
+    return 0;
+}
+
+void opr(){
+    cout << "opr" << endl;
+    cin >> ch;
+    if (ch == 'e'){
+        enq();
+    }else if(ch == 'd'){
+        deq();
+    }else{
+        dis();
+    }
+}
+
+void enq(){
+    if(front == -1 && rear == -1){
+        front = rear = 0;
+        cout << "Item" << endl;
+        cin >> queue[rear];
+    }else{
+        if((rear+1)%MAX == front){
+            cout << "full" << endl;
+        }else{
+            cout << "Item" << endl;
+            cin >> queue[++rear];
+        }
+        
+    }
+    opr();
+}
+void deq(){
+    if(front == -1 && rear == -1){
+        cout << "empty" << endl;
+    }else if (front == rear){
+        front = rear = -1;
+    }else{
+        front = (front+1)%MAX;
+    }
+    opr();
+}
+void dis(){
+    i = front;
+    while(i != (rear+1)%MAX){
+        cout << queue[i] << " ";
+        i = (i+1)%MAX;
+    }
+    cout << endl;
+    opr();
+}
